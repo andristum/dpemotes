6f91a0227c7d1e91f1cb526a3bdd2bdedc0737e4
andristum <47616367+andristum@users.noreply.github.com>
2019-10-24 07:26:21 +0200

    1.2 Move things around to make it way more neat

---
+++ Config.lua
@@ -0,0 +1,11 @@
+Config = {
+-- Set this to true to enable some extra prints
+	DebugDisplay = false,
+-- Set this to false if you have something else on X, and then just use /e c to cancel emotes.
+	EnableXtoCancel = true,
+-- Set this to true if you want to disarm the player when they play an emote.
+	DisarmPlayer= false,
+-- You can disable the (F3) menu here / change the keybind
+	MenuKeybindEnabled = true,
+	MenuKeybind = 170 -- Get the button number here https://docs.fivem.net/game-references/controls/
+}
