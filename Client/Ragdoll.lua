abe62459ff3f7d041a576a8e603358e844b59421
andristum <47616367+andristum@users.noreply.github.com>
2019-10-26 10:06:53 +0200

    1.4.2a update ragdoll more smoothly

--- Client/Ragdoll.lua
+++ Client/Ragdoll.lua
@@ -2,7 +2,7 @@ local isInRagdoll = false
 
 Citizen.CreateThread(function()
  while true do
-    Citizen.Wait(100)
+    Citizen.Wait(10)
     if isInRagdoll then
       SetPedToRagdoll(GetPlayerPed(-1), 1000, 1000, 0, 0, 0, 0)
     end
