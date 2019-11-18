bde8e8992f152ee2962bb4511d005cabf7ecde54
andristum <47616367+andristum@users.noreply.github.com>
2019-11-18 06:38:48 +0100

    Update Updates.lua

--- Server/Updates.lua
+++ Server/Updates.lua
@@ -10,12 +10,14 @@ function checkVersion(err,responseText, headers)
     curVersion = LoadResourceFile(GetCurrentResourceName(), "version")
 
     if curVersion ~= responseText and tonumber(curVersion) < tonumber(responseText) then
-        print("\n------------------------------------------------")
-        print("\n"..resourceName.." is outdated, latest version is:\n"..responseText.."yours:\n"..curVersion.."\nupdate from https://github.com"..updatePath.."")
-        print("\n------------------------------------------------")
+        print("\n^1----------------------------------------------------------------------------------^7")
+        print(resourceName.." is outdated, latest version is: ^2"..responseText.."^7, installed version: ^1"..curVersion.."^7!\nupdate from https://github.com"..updatePath.."")
+        print("^1----------------------------------------------------------------------------------^7")
     elseif tonumber(curVersion) > tonumber(responseText) then
         print("You somehow skipped a few versions of "..resourceName.." or the git went offline, if it's still online i advise you to update ( or downgrade? )")
     else
-        print("\n"..resourceName.." is up to date. ("..curVersion..")")
+        print("\n^2-------------------------------------------^7")
+        print("\n"..resourceName.." is up to date. (^2"..curVersion.."^7)")
+        print("\n^2-------------------------------------------^7")
     end
 end
