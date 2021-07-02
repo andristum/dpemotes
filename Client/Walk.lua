0ad6964f1386167a45ddd8d30dd89d1a4b95d11c
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2021-07-02 20:45:12 +0200

    Add walkstick event

--- Client/Walk.lua
+++ Client/Walk.lua
@@ -34,3 +34,32 @@ function WalkCommandStart(source, args, raw)
     EmoteChatMessage("'"..name.."' is not a valid walk")
   end
 end
+
+local WalkstickUsed = false
+local WalkstickObject = nil
+
+RegisterNetEvent('dp:Client:UseWalkingStick')
+AddEventHandler('dp:Client:UseWalkingStick', function()
+  if not WalkstickUsed then
+    local ped = PlayerPedId()
+    RequestAnimSet('move_heist_lester')
+    while not HasAnimSetLoaded('move_heist_lester') do
+      Citizen.Wait(1)
+    end
+    SetPedMovementClipset(ped, 'move_heist_lester', 1.0) 
+    WalkstickObject = CreateObject(GetHashKey("prop_cs_walking_stick"), 0, 0, 0, true, true, true)
+    AttachEntityToEntity(WalkstickObject, ped, GetPedBoneIndex(ped, 57005), 0.16, 0.06, 0.0, 335.0, 300.0, 120.0, true, true, false, true, 5, true)
+  else
+    local ped = PlayerPedId()
+    if PreviousWalkset ~= nil then
+      RequestAnimSet(PreviousWalkset)
+      while not HasAnimSetLoaded(PreviousWalkset) do
+        Citizen.Wait(1)
+      end
+      SetPedMovementClipset(ped, PreviousWalkset, 1.0)
+    end
+    DetachEntity(WalkstickObject, 0, 0)
+    DeleteEntity(WalkstickObject)
+  end
+  WalkstickUsed = not WalkstickUsed
+end)
