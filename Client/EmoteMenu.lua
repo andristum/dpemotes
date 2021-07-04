b36a7ee29e5ac8c427460360cd465981c1ffcadb
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2021-07-04 14:05:43 +0200

    Remove old GetObject

--- Client/EmoteMenu.lua
+++ Client/EmoteMenu.lua
@@ -1,12 +1,3 @@
-QBCore = nil
-
-Citizen.CreateThread(function() 
-  while QBCore == nil do
-    	TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
-    	Citizen.Wait(200)
-  end
-end)
-
 TriggerServerEvent("dp:CheckVersion")
 
 rightPosition = {x = 1450, y = 100}
