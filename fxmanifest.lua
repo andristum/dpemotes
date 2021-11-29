7dbc9b88c5ff46ee17bbea8165f5159dbb08f92d
kibukj <kibuzhj@gmail.com>
2021-11-29 16:22:32 -0500

    Replace ghmattimysql with oxmysql

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ fx_version 'adamant'
 game 'gta5'
 
 -- Comment this out if you don't want to use the SQL keybinds
-dependency 'ghmattimysql'
+dependency 'oxmysql'
 
 client_scripts {
 	'NativeUI.lua',
